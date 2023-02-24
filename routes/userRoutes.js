5b86fefd11af51d795a35d6aeb3287f60cfbb6d1
A-60 Harshal Maheshram <harshalmaheshram10@gmail.com>
2023-02-25 00:41:23 +0530

    api updated

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,3 +1,5 @@
+const userID = require('./middlewares/userId.js')
+
 const registerController = require('../controllers/userController');
 
 const router = require('express').Router();
@@ -5,6 +7,8 @@ const router = require('express').Router();
 router.post('/register',registerController.register)
 
 router.get('/login',registerController.login)
+
+router.get('/sendUserId',userID)
 
 
 module.exports = router
